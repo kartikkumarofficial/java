7e62b88d20ef50bf4be662b6ef252182ff9b765a
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-06 13:19:43 +0530

    Patterns

--- for loop/Pattern3.java
+++ for loop/Pattern3.java
@@ -1,12 +1,12 @@
-public class Pattern1{
+public class Pattern3 {
     public static void main(String[] args) {
-        for (int i = 1 ; i<=5 ; i++){
-            for(int j = 1 ;j<=i;j++ ){
+        for ( int i=5 ; i>=1 ; i--){
+            for (int j=1;j<=i;j++){
                 System.out.print(j + " ");
             }
             System.out.println();
-
         }
-
+        
     }
+    
 }
