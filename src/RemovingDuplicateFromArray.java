b32fb5c518262284f585f0c1b3ace86c703f5d92
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-15 13:05:59 +0530

    removing duplicate elements

---
+++ src/RemovingDuplicateFromArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class RemovingDuplicateFromArray {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,2,553,11,2,3,4,4,1};
+        int[] nondup = new int[arr.length];
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length-1 ; j++) {
+                if(arr[i]==arr[j]){
+
+
+
+                }
+
+
+            }
+        }
+
+            }
+        }
