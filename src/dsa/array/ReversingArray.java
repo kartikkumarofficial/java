c720037ebcf473e375a7a189e033082e331ff401
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-08-11 13:26:05 +0530

    java

--- src/dsa/array/ReversingArray.java
+++ src/dsa/array/ReversingArray.java
@@ -15,9 +15,10 @@ public class ReversingArray {
 //    }
 public static void reverse(int[] arr) {
     for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+        arr[i]+=arr[j];
+        arr[j]=arr[i]-arr[j];
+        arr[i]=arr[i]-arr[j];
+
     }
 }
 
