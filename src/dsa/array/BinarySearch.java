717d138b7ece939c30f398f6deebea61c6679b74
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-08-12 20:42:58 +0530

    java

--- src/dsa/array/BinarySearch.java
+++ src/dsa/array/BinarySearch.java
@@ -11,16 +11,22 @@ public class BinarySearch {
 //            int mid = (start+end)/2; //since integers in java have limited value and if end exceeds that the code might break here so better way to do that is below
             int mid = start + (end-start)/2;
 
-
-
-
+            if(target<nums[mid]){
+                end=mid-1;
+            }else if (target> nums[mid]){
+                start=mid + 1;
+            }else{
+                return mid;
+            }
         }
 
 
-        return 0;
+        return -1; //will only execute if value not found
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9};
+        int ans = binSearch(arr,6);
+        System.out.println(ans);
 //        binary search
 
     }
