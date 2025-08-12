c9eb4b40978e0f2ca83252229ab25795955fd6c2
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-08-12 19:12:41 +0530

    java

---
+++ src/dsa/array/BinarySearch.java
@@ -0,0 +1,27 @@
+package dsa.array;
+
+public class BinarySearch {
+    public static int binSearch(int[] nums,int target){
+//        sort the array
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end){
+            //find the middle element
+//            int mid = (start+end)/2; //since integers in java have limited value and if end exceeds that the code might break here so better way to do that is below
+            int mid = start + (end-start)/2;
+
+
+
+
+        }
+
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+//        binary search
+
+    }
+}
