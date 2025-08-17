cf85e02d6665b51915a74727036d259d71105020
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-08-17 23:24:16 +0530

    java

--- src/dsa/array/OrderAgnosticBinarySearch.java
+++ src/dsa/array/OrderAgnosticBinarySearch.java
@@ -1,47 +1,53 @@
 package dsa.array;
-//start>end==descending
-//end>start==ascending
+
 public class OrderAgnosticBinarySearch {
-    public static int BinarySearch(int[] nums, int target){
+    public static int BinarySearch(int[] nums, int target) {
         int start = 0;
-        int end =nums.length-1;
+        int end = nums.length - 1;
 
-        if (nums[start]>nums[end]){//descending
-            while (start<=end){
-                int middle = start + (end-start)/2;
+        // handle array with all equal elements
+        if (nums[start] == nums[end]) {
+            if (nums[start] == target) return start;
+            return -1;
+        }
 
-                if(target==nums[middle]){
-                    return middle;
-                } else if (target>nums[middle]) {
-                    end=middle-1;
+        boolean isAscending = nums[start] < nums[end]; // order detection
 
-                }else if (target<nums[middle]){
-                    start=middle+1;
-                }
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
             }
-        }else if (nums[start]<nums[end]){
-            while (start<=end){
-                int middle = start + (end-start)/2;
 
-                if(target==nums[middle]){
-                    return middle;
-                } else if (target>nums[middle]) {
-                    start=middle+1;
-
-                }else if (target<nums[middle]){
-                    end=middle-1;
+            if (isAscending) { // ascending case
+                if (target < nums[middle]) {
+                    end = middle - 1;
+                } else {
+                    start = middle + 1;
+                }
+            } else { // descending case
+                if (target > nums[middle]) {
+                    end = middle - 1;
+                } else {
+                    start = middle + 1;
                 }
             }
         }
-        return -1;
-
-
+        return -1; // not found
     }
 
     public static void main(String[] args) {
-        int[] nums = {7,6,3,2,1};
-        int target = 3;
-        System.out.println(BinarySearch(nums,target));
+        int[] nums1 = {7, 6, 3, 2, 1}; // descending
+        int target1 = 3;
+        System.out.println(BinarySearch(nums1, target1)); // 2
 
+        int[] nums2 = {1, 2, 3, 4, 5}; // ascending
+        int target2 = 4;
+        System.out.println(BinarySearch(nums2, target2)); // 3
+
+        int[] nums3 = {2, 2, 2, 2}; // all equal
+        int target3 = 2;
+        System.out.println(BinarySearch(nums3, target3)); // 0
     }
 }
