b2a39c4a98781bd659215edd50d9c1ab1642cd82
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-08-19 22:19:37 +0530

    java

---
+++ src/dsa/array/BinarySearch/FloorOfANum.java
@@ -0,0 +1,47 @@
+package dsa.array.BinarySearch;
+
+public class FloorOfANum {
+//    floor - smallest num that is greater or equal to target number
+
+    public static int Floor(int nums[],int target){
+
+        int start = 0;
+        int end = nums.length;
+        if (nums[start]==nums[end]&nums[end]==target){
+            return nums[start];
+        }
+        boolean isAscending = nums[start]<nums[end];
+        while(start<=end){
+            int middle = start + (end-start)/2;
+
+            if (nums[middle]==target){
+                return middle;
+
+            }
+
+            if (isAscending){
+                if(nums[middle]<target){
+                    end=middle-1;
+                }else if(nums[middle]>target) {
+                    start = middle+1;
+                }
+
+            }
+
+
+
+        }
+
+
+
+        return nums[end];
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1,2,4,5,7,8,9};
+        int target = 3;
+//        Floor obj = new Floow();
+        System.out.println(Floor(nums,target));
+    }
+
+}
