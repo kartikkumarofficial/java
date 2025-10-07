197ecbfd5efcf52b3d217e61aae3b52eca060411
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-10-07 16:22:01 +0530

    java

--- src/Revision/BinarySearchRev2.java
+++ src/Revision/BinarySearchRev2.java
@@ -9,16 +9,19 @@ public class BinarySearchRev2 {
     public static int binarySearch(int[] nums,int target){
         int start = 0;
         int end = nums.length-1;
-
+        boolean isAsc = nums[start]<nums[end]; //making it order agnostic
 
             while(start<=end){
                 int middle = start + (end-start)/2 ;
-                if(target==nums[middle]){
+                if(target==nums[middle])
                     return middle;
-                }else if (target<nums[middle]){
-                    end=middle-1;
-                }else if (target>nums[middle]){
-                    start = middle+1;
+
+                if(isAsc){
+                    if(target>nums[middle]) start = middle+1;
+                    if(target<nums[middle]) end = middle-1;
+                }else {
+                    if(target>nums[middle]) end = middle-1;
+                    if(target<nums[middle]) start = middle+1;
                 }
             }
 
