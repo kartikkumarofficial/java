98c3f6f66fd3008d47d008006a66496348ed2f58
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-10-11 18:41:48 +0530

    java

---
+++ src/dsa/array/sorting/InsertionSort.java
@@ -0,0 +1,20 @@
+package dsa.array.sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,4,1,2};
+
+    }
+    public static int[] insertionSort(int[] nums){
+        int n = nums.length-2;
+        for (int i = 0; i <= n; i++) {
+
+
+            for (int j = 0; j <= n; j++) {
+
+            }
+        }
+
+        return nums;
+    }
+}
