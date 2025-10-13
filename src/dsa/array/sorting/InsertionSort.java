06c1baf180d15a6b2bbf93eb22e8efdccaa927b8
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-10-13 17:48:33 +0530

    java

--- src/dsa/array/sorting/InsertionSort.java
+++ src/dsa/array/sorting/InsertionSort.java
@@ -1,20 +1,28 @@
 package dsa.array.sorting;
+import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {3,5,4,1,2};
-
+        System.out.println(Arrays.toString(insertionSort(arr)));
     }
+
     public static int[] insertionSort(int[] nums){
-        int n = nums.length-2;
-        for (int i = 0; i <= n; i++) {
 
-
-            for (int j = 0; j <= n; j++) {
-
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j >0; j--) {
+                if(nums[j]<nums[j-1]){
+                    swap(nums,j,j-1);
+                }else{
+                    break;
+                }
             }
         }
-
         return nums;
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
