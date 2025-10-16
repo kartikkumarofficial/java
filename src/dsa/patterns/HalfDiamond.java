f3e8568381ca9f75d9a83ef7a4ecacd10bde18ec
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-10-16 21:37:25 +0530

    java

---
+++ src/dsa/patterns/HalfDiamond.java
@@ -0,0 +1,22 @@
+package dsa.patterns;
+
+public class HalfDiamond {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("*");
+                
+            }
+            
+        }
+    }
+}
+
+
+//        1
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5
+
