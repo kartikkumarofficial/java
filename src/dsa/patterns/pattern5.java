af9819f8dea6ae7d14ae3612db515d01178ec7b5
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-10-18 22:27:59 +0530

    java

--- src/dsa/patterns/pattern5.java
+++ src/dsa/patterns/pattern5.java
@@ -1,26 +1,45 @@
 package dsa.patterns;
+//
+//public class pattern5 {
+//    public static void main(String[] args) {
+//        int n = 5;
+//        for (int row = 1; row <= (2*n-1); row++) {
+//            if(row<=n){
+//                for (int col = 1; col <= row; col++) {
+//                    System.out.print("* ");
+//                }
+//                }else if (row>n){
+//                    for (int col = (2*n)-row; col>=1; col--) {
+//                        System.out.print("* ");
+//
+//
+//
+//                }
+//            }
+//            System.out.println(" ");
+//        }
+//
+//
+//    }
+//}
+
+
+//stream builder way to achieve this
 
 public class pattern5 {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
         int n = 5;
-        for (int row = 1; row <= (2*n-1); row++) {
-            if(row<=n){
-                for (int col = 1; col <= row; col++) {
-                    System.out.print("* ");
-                }
-                }else if (row>n){
-                    for (int col = (2*n)-row; col>=1; col--) {
-                        System.out.print("* ");
+        for (int row = 1; row <= (2 * n - 1); row++) {
+            if(row<=n)sb.append("* ");
+            else sb.delete(sb.length()-2,sb.length());
 
-
-
-                }
-            }
-            System.out.println(" ");
+            System.out.println(sb);
         }
+
+
+
 
 
     }
 }
-
-
