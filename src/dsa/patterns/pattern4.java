7501e1c9f784fbb932d7f0fb96cbbc129001166c
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-10-18 09:16:46 +0530

    java

---
+++ src/dsa/patterns/pattern4.java
@@ -0,0 +1,19 @@
+package dsa.patterns;
+//half diamond
+public class pattern4 {
+    public static void main(String[] args) {
+        for (int row = 1; row <6; row++) {
+            for (int col = 1; col <=row; col++) {
+                System.out.print(col+ " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ src/dsa/patterns/pattern5.java
@@ -0,0 +1,19 @@
+package dsa.patterns;
+
+public class pattern5 {
+    public static void main(String[] args) {
+
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        //after this i want it to loop back
+        //approach 1 is again make 2 loops that will do
+
+
+    }
+}
+
+
