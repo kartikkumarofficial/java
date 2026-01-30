a8033eb804be5acf20e1605893cbda976be84df9
kartikkumarofficial <work.kartikkumar@gmail.com>
2026-01-30 21:29:11 +0530

    quick sort

---
+++ src/dsa/array/sorting/QuickSort.java
@@ -0,0 +1,53 @@
+package dsa.array.sorting;
+
+public class QuickSort {
+
+    public static int partition(int[] arr , int low ,int high){
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for(int j = low; j<high ; j++ ){
+            if (arr[j]< pivot ){
+                i++;
+
+                //swap
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+
+            }
+        }
+        //place the pivot at its desired location -> swap arr[i+1] with pivot
+
+        int temp = arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high]= temp;
+
+
+
+        return  i+ 1;
+    }
+
+
+    public static void quickSort(int[] arr , int low , int high){
+        if(low<high){
+            int p = partition(arr, low ,high);
+            quickSort(arr , low , p-1);
+            quickSort(arr , p+1,high );
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,4,7,6,9,2,3,1,8};
+        quickSort(arr,0,arr.length-1);
+
+        for(int i =0;i<arr.length ; i++ ){
+            System.out.println(i + " ");
+        }
+
+    }
+
+}
+
+
