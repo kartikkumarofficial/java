1dff8c792538e1b3db678973ad45e7c4be5880ca
kartikkumarofficial <work.kartikkumar@gmail.com>
2026-03-20 23:16:54 +0530

    removealloccur

--- src/clg/remove_all_occur.java
+++ src/clg/remove_all_occur.java
@@ -4,7 +4,6 @@ public class remove_all_occur {
     public static void main(String[] args) {
         String s = "The bookkeeper has a book about bookkeeping in his bookshop";
         String x = "book";
-
         String result = s.replace(x, "");
         System.out.println(result);
     }
