95ff273d6d061742a6a2a1e28d7d23ae088d2185
kartikkumarofficial <work.kartikkumar@gmail.com>
2026-05-22 22:22:44 +0530

    bitwise

---
+++ src/dsa/bitwise/power.java
@@ -0,0 +1,19 @@
+package dsa.bitwise;
+
+public class power {
+    public static void main(String[] args) {
+        System.out.println(powerr(2,5));
+    }
+    public static int powerr(int base , int power){
+        int n = 1;
+        int b = base;
+        while(power>0){
+            if((power&1)==1){
+                n*= b;
+            }
+            power=power>>1;
+            b*=b;
+        }
+        return n;
+    }
+}
