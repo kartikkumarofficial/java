82af09cbcbffb69a68e065177c6078920734f926
Kartik Kumar <work.kartikkumar@gmail.com>
2026-06-04 13:45:49 +0530

    recursion

---
+++ src/dsa/recursion/arrays/merge_sort.java
@@ -0,0 +1,53 @@
+package dsa.recursion.arrays;
+
+import java.util.Arrays;
+
+public class merge_sort {
+    public static void main(String[] args) {
+        int[] arr = {8,3,4,12,5,6};
+        System.out.println(Arrays.toString(mergesort(arr)));;
+
+    }
+
+    private static int[] mergesort(int[] arr) {
+        if(arr.length==1){
+            return arr;
+        }
+        int mid =arr.length/2;
+
+        int[] left = mergesort(Arrays.copyOfRange(arr,0,mid));
+        int[] right = mergesort(Arrays.copyOfRange(arr,mid,arr.length));
+
+        return merge(left , right);
+
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] mix = new int[left.length + right.length];
+        int i = 0,j=0,k=0;
+        while(i<left.length && j<right.length){
+            if (left[i]<right[j]){
+                mix[k] = left[i];
+                i++;
+                k++;
+            }else{
+                mix[k]=right[j];
+                j++;
+                k++;
+            }}
+            while (i<left.length){
+                mix[k]=left[i];
+                i++;
+                k++;
+            }
+            while (j<right.length){
+                mix[k]=right[j];
+                j++;
+                k++;
+            }
+
+
+        return mix;
+
+    }
+}
